707ab59397877945d87a8d5043c23b3b8335a20a
CETECOM <hec.salas@duocuc.cl>
2025-08-25 11:40:37 -0400

    Activity 3 - Sin terminar

--- app/src/main/java/com/example/miproyectobase2/MainActivity2.kt
+++ app/src/main/java/com/example/miproyectobase2/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.miproyectobase2
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,11 +14,20 @@ class MainActivity2 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
 
+        //ACTIVITY DESTINO
         val mensajeBienvenida:TextView = findViewById(R.id.tx_bienvenido)
+        val btnFormulario:Button = findViewById(R.id.btnFormulario)
+
+        //CREO VARIABLE ASIGNO VALOR DESDE OTRO ACTIVITY
         val usuarioDesdeotroActivity = intent.getStringExtra("pUsuario")
 
+        //SETEO UN TEXT VIEW REEMPLAZANDO EL TEXTO POR EL CONTENIDO
         mensajeBienvenida.text = usuarioDesdeotroActivity.toString()
 
+        val recibeClave = intent.getStringExtra("par_clave")
+
+        //ACCION SOBRE EL BOTON
+        btnFormulario.setOnClickListener()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/miproyectobase2/MainActivity3.kt
+++ app/src/main/java/com/example/miproyectobase2/MainActivity3.kt
@@ -1,6 +1,8 @@
 package com.example.miproyectobase2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,16 @@ class MainActivity3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
+
+        //declaramos variables con los elementos del layout
+        val edNombre:EditText = findViewById(R.id.edNombre)
+        val edApellido:EditText = findViewById(R.id.edApellido)
+        val edFechaNac:EditText = findViewById(R.id.edFechaNac)
+        val btnGuardar:Button = findViewById(R.id.btnGuardar)
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
