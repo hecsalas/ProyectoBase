4e4c84734b422f74e194c03da4d8fdfa01c94ea4
obicarrasco <ni.carrascoa@duocuc.cl>
2025-08-27 11:49:55 -0400

    Operaciones matematicas + spinner + try catch

--- app/src/main/java/com/example/miproyectobase2/utils/OpMatematicas.kt
+++ app/src/main/java/com/example/miproyectobase2/utils/OpMatematicas.kt
@@ -12,4 +12,13 @@ object OpMatematicas {
         return n1 - n2
     }
 
+    fun dividir(n1: Int, n2: Int): Int {
+        try {
+            return n1 / n2
+        }catch (e: ArithmeticException){
+            return 0
+        }finally {
+            println("Se ha producido un error")
+        }
+    }
 }
